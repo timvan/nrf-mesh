bdf4662ed3173c9afa0a0a95b3a857513551bd98
timvan <timvanellemeet@gmail.com>
2019-07-04 14:40:30 +0100

    hmm. pre demo

--- scripts/interactive_pyaci/node-red-contrib-ble-mesh/ble-mesh.js
+++ scripts/interactive_pyaci/node-red-contrib-ble-mesh/ble-mesh.js
@@ -22,12 +22,15 @@ function onProvisionComplete() {
     var nodeId = 0;
 };
 
+function provision() {
+    var uuid = unProvisionedBleNodes.pop().uuid;
+    pyaci.provision(onProvisionComplete, uuid);
+};
+
 function onDiscover(data) {
     unProvisionedBleNodes.push(data);
     console.log(`Nodes ${unProvisionedBleNodes}`, JSON.stringify(unProvisionedBleNodes));
-
-    var uuid = unProvisionedBleNodes.pop().uuid;
-    pyaci.provision(onProvisionComplete, uuid);
+    provision();
 };
 
 setTimeout(() => {
@@ -50,7 +53,11 @@ module.exports = function(RED) {
 
         var node = this;
         this.uuid = "9db77a0526b8734988639509c242d107";
-        this.pin = 12;
+        
+        // this.pin = 18;
+        if(config.pin){
+            this.pin = config.pin;
+        }
         
         pyaci.configureGPIO(false, this.pin, this.uuid);
 
@@ -72,7 +79,11 @@ module.exports = function(RED) {
 
         var node = this;
         this.uuid = "9db77a0526b8734988639509c242d107";
-        this.pin = 13;
+        
+        // this.pin = 12;
+        if(config.pin){
+            this.pin = config.pin;
+        }
 
         pyaci.configureGPIO(true, this.pin, this.uuid);
 
@@ -114,6 +125,12 @@ module.exports = function(RED) {
     RED.httpAdmin.get('/__bleMeshUnProveDevList', (req, res) => {
         body = unProvisionedBleNodes;
         RED.log.info('/__bleMeshUnProveDevList', JSON.stringify(body));
+        res.json(body);
+    });
+
+    RED.httpAdmin.get('/__bleMeshDevList', (req, res) => {
+        body = bleNodes;
+        RED.log.info('/__bleMeshDevList', JSON.stringify(body));
         res.json(body);
     });
 }
